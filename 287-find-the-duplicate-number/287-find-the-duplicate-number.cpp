f8e77c12109892fcd9230d3f0d708f08c2221398
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-09 17:40:49 +0530

    Time: 200 ms (42.28%), Space: 61.1 MB (91.15%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // Brute Force approach
+        /********* Brute Force approach **********/
         // Time complexity-O(nlogn), Space complexity-O(1)
         
         // int len = nums.size();
@@ -13,18 +13,40 @@ public:
         // }
         // return -1;
         
-        // Better approach
+        
+        /********* Better approach ***********/
         // Time complexity-O(n), Space complexity-O(n)
-        int len = nums.size();
-        vector<int>freq(len,0);
-        for(int i=0;i<len;i++){
-            if(freq[nums[i]]==0){
-                freq[nums[i]]++;
-            }
-            else{
-                return nums[i];
-            }
-        }
-        return -1;
+        
+        // int len = nums.size();
+        // vector<int>freq(len,0);
+        // for(int i=0;i<len;i++){
+        //     if(freq[nums[i]]==0){
+        //         freq[nums[i]]++;
+        //     }
+        //     else{
+        //         return nums[i];
+        //     }
+        // }
+        // return -1;
+        
+        
+        /******** Optimal approach *********/
+        // Time complexity-O(n), Space complexity-O(1)
+        // Linked List cycle method(Tortoise/runner method)
+        
+        int slow=nums[0];
+        int fast=nums[0];
+        do{
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+        }while(slow!=fast);
+        
+        fast=nums[0];
+       while(slow!=fast){
+           slow=nums[slow];
+           fast=nums[fast];
+       }
+        
+        return slow;
     }
 };
