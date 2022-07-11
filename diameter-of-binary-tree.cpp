3cce5830c368a6009b75faa9cef4e1456ef32996
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-11 15:52:41 +0530

    Create diameter-of-binary-tree.cpp

---
+++ diameter-of-binary-tree.cpp
@@ -0,0 +1,49 @@
+// Diameter of binary tree is defined as "The longest path between any two nodes."
+
+// Naive approach
+// Time complexity - O(N^2)
+// Space complexity - auxilliary space - O(N)
+
+int heightOfBinaryTree(TreeNode* root){
+  if(root==NULL)
+    return 0;
+  int lh=heightOfBinaryTree(root->left);
+  int rh=heightOfBinaryTree(root->right);
+  return max(lh,rh)+1;
+}
+
+void diameter(TreeNode*root,int &maxVariable){
+  if(root==NULL)
+    return;
+  int lh=heightOfBinaryTree(root->left);
+  int rh=heightOfBinaryTree(root->right);
+  maxVariable=max(maxVariable,lh+rh);
+  diameter(root->left,maxVariable);
+  diameter(root->right,maxVariable);
+}
+
+int diameterOfBinaryTree(TreeNode* root){
+  int maxi=0;
+  diameter(root,maxi);
+  return maxi;
+}
+
+
+// Optimal approach
+// Time complexity - O(N)
+
+int findDiameter(TreeNode* root, int &diameter){
+  if(root==NULL)
+    return 0;
+  int lh=findDiameter(root->left,diameter);
+  int rh=findDiameter(root->right,diameter);
+  diameter=max(diameter,lh+rh);
+  return 1+max(lh,rh);
+}
+
+int diameterOfBinaryTree(TreeNode* root){
+  int diameter=0;
+  findDiameter(root,diameter);
+  return diameter;
+}
+
