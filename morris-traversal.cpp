374186c2e1a5f5ae7b5110c6dd085c0785cbcf1f
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-14 00:05:33 +0530

    Create morris-traversal.cpp

---
+++ morris-traversal.cpp
@@ -0,0 +1,4 @@
+// Time complexity - O(N)
+// Space complexity - O(1)
+// Morris traversal uses the concept of 'Threaded Binary Tree'.
+// In recursive implementation, backtracking takes care of the returning.
