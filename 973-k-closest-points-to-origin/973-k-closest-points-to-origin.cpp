7c863c8599d37f7b81fccd6af10a3b1b1e34b078
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-22 05:01:12 +0530

    Time: 180 ms (99.27%), Space: 57.1 MB (89.05%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.cpp
@@ -2,20 +2,26 @@ class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         // O(nlogq)
-        priority_queue<pair<int,vector<int>>>pq;
-        for(auto point=0;point<points.size();point++){
-            int f = points[point][0];
-            int l = points[point][1];
-            int d = (f*f)+(l*l);
-            pq.push({d,points[point]});
-            if(pq.size()>k) pq.pop();
-        }
-        vector<vector<int>>ans;
-        while(!pq.empty()){
-            ans.push_back(pq.top().second);
-            pq.pop();
-        }
-        return ans;
+        // priority_queue<pair<int,vector<int>>>pq;
+        // for(auto point=0;point<points.size();point++){
+        //     int f = points[point][0];
+        //     int l = points[point][1];
+        //     int d = (f*f)+(l*l);
+        //     pq.push({d,points[point]});
+        //     if(pq.size()>k) pq.pop();
+        // }
+        // vector<vector<int>>ans;
+        // while(!pq.empty()){
+        //     ans.push_back(pq.top().second);
+        //     pq.pop();
+        // }
+        // return ans;
+        
+         nth_element(begin(points), begin(points) + k, end(points), [](vector<int> &a, vector<int> &b) {
+      return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1];
+  });
+  points.resize(k);
+  return points;
         
     }
 };
