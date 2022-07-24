c7992898d0a444fbe3c6e560aa06697b726dcf7f
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-24 19:39:38 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (54.60%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,27 +1,46 @@
 class Solution {
 public:
     
-    void permuteHelper(vector<int>& nums, int n, vector<int>& ds, map<int,int>& mpp, vector<vector<int>>&res){
-        if(ds.size() == n) {
-            res.push_back(ds);
+//     void permuteHelper(vector<int>& nums, int n, vector<int>& ds, map<int,int>& mpp, vector<vector<int>>&res){
+//         if(ds.size() == n) {
+//             res.push_back(ds);
+//             return;
+//         }
+//         for(int i=0;i<n;i++){
+//             if(!mpp[i]){
+//                 mpp[i]=1;
+//                 ds.push_back(nums[i]);
+//                 permuteHelper(nums,n,ds,mpp,res);
+//                 mpp[i]=0;
+//                 ds.pop_back();
+//             }
+//         }
+//     }
+    
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>res;
+//         map<int,int>mpp;
+//         vector<int>ds;
+//         permuteHelper(nums,nums.size(),ds,mpp,res);
+//         return res;
+//     }
+    
+    void permuteHelper(vector<int>& nums, int idx, vector<vector<int>>& res){
+        if(idx == nums.size()){
+            res.push_back(nums);
             return;
         }
-        for(int i=0;i<n;i++){
-            if(!mpp[i]){
-                mpp[i]=1;
-                ds.push_back(nums[i]);
-                permuteHelper(nums,n,ds,mpp,res);
-                mpp[i]=0;
-                ds.pop_back();
-            }
+        
+        for(int i=idx;i<nums.size();i++){
+            swap(nums[idx],nums[i]);
+            permuteHelper(nums,idx+1,res);
+            swap(nums[idx],nums[i]);
         }
     }
     
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> permute(vector<int> &nums) {
         vector<vector<int>>res;
-        map<int,int>mpp;
-        vector<int>ds;
-        permuteHelper(nums,nums.size(),ds,mpp,res);
+        permuteHelper(nums,0,res);
         return res;
     }
 };
