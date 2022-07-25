e7996140038707ceef40c3908add2cdc48f8938b
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-25 23:36:23 +0530

    Update Rat-in-a-maze-I.cpp

--- Rat-in-a-maze-I.cpp
+++ Rat-in-a-maze-I.cpp
@@ -47,6 +47,41 @@ class Solution{
         if(m[0][0]==1) solve(0,0,m,n,ans,visited,"");
         return ans;
     }
+    
+    
+    
+    
+    
+    
+    // Less repetitive code
+    void solve(int i,int j,vector<vector<int>>& m,int n,int di[],int dj[],vector<string>& ans,vector<vector<int>>& visited,string move){
+        if(i==n-1 && j==n-1){
+            ans.push_back(move);
+            return;
+        }
+        string dir="DLRU";
+        for(int idx=0;idx<4;idx++){
+            int nexti = i+di[idx];
+            int nextj = j+dj[idx];
+            if(nexti>=0 && nextj>=0 && nexti<n && nextj<n && visited[nexti][nextj]==0 && m[nexti][nextj]==1){
+                visited[i][j]=1;
+                solve(nexti,nextj,m,n,di,dj,ans,visited,move+dir[idx]);
+                visited[i][j]=0;
+            }
+        }
+
+    }
+    
+    
+    vector<string> findPath(vector<vector<int>>& m,int n) {
+        vector<string>ans;
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        int di[] = {1,0,0,-1};
+        int dj[] = {0,-1,1,0};
+        if(m[0][0]==1) solve(0,0,m,n,di,dj,ans,visited,"");
+        return ans;
+    }
+};
 };
 
     
