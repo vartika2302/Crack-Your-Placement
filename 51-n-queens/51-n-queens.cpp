87c68587b3526af05b762ad1173eb2705d9fc879
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-25 07:20:14 +0530

    Update 51-n-queens.cpp

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -56,4 +56,40 @@ public:
         solve(0,res,board,n);
         return res;
     }
+    
+    
+    
+     // Optimal Solution
+    
+    void solve(int col,vector<string>& board, vector<vector<string>>& res, vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal, int n){
+        if(col == n){
+            res.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0){
+                board[row][col]='Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1+col-row] = 1;
+                solve(col+1,board,res,leftRow,lowerDiagonal,upperDiagonal,n);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] = 0;
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n){
+        vector<vector<string>> res;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i] = s;
+        }
+        vector<int> leftRow(n,0), upperDiagonal((2*n)-1,0), lowerDiagonal((2*n)-1,0);
+        solve(0,board,res,leftRow,lowerDiagonal,upperDiagonal,n);
+        return res;
+    }
 };
