bb6f2dab22701289eefe050f83890d3062111af6
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-26 20:47:43 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (83.64%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -26,14 +26,25 @@ public:
     
     // TABULATION - BOTTOM-UP
     
-    int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        dp[0]=1;
-        dp[1]=1;
+//     int climbStairs(int n) {
+//         vector<int>dp(n+1,-1);
+//         dp[0]=1;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i] = dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+    
+    int climbStairs(int n){
+        int prev=1;
+        int prev2=1;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            int curri = prev+prev2;
+            prev2=prev;
+            prev=curri;
         }
-        return dp[n];
+        return prev;
     }
     
 };
