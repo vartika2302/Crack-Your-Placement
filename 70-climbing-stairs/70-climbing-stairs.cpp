40d2a293591c96f351522359e70a36f263c33096
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-26 20:33:25 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    // RECURSION
     // int climbStairs(int n) {
     //     if(n==0) return 1;
     //     if(n==1) return 1;
@@ -8,14 +10,30 @@ public:
     //     return first+second;
     // }
     
-    int solve(int n,vector<int>& dp){
-        if(n<=1) return 1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+    
+    // MEMOIZATION - TOP-DOWN
+//     int solve(int n,vector<int>& dp){
+//         if(n<=1) return 1;
+//         if(dp[n]!=-1) return dp[n];
+//         return dp[n] = solve(n-1,dp)+solve(n-2,dp);
+//     }
+    
+//     int climbStairs(int n){
+//         vector<int>dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+    
+    
+    // TABULATION - BOTTOM-UP
+    
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
     
-    int climbStairs(int n){
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
-    }
 };
