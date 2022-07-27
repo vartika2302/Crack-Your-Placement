e46d713ef36ae55f083fe5794acaf0ed7830798f
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-27 17:48:16 +0530

    Time: 0 ms (100.00%), Space: 6 MB (29.83%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -28,19 +28,35 @@ public:
 //     }
     
     // TABULATION
-    int solve(int n,vector<int>& dp){
-        dp[0]=0;
-        if(n>=1) dp[1]=1;
-        if(n>=2) dp[2]=1;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
-        }
-        return dp[n];
-    }
+//     int solve(int n,vector<int>& dp){
+//         dp[0]=0;
+//         if(n>=1) dp[1]=1;
+//         if(n>=2) dp[2]=1;
+//         for(int i=3;i<=n;i++){
+//             dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+//         }
+//         return dp[n];
+//     }
     
+//     int tribonacci(int n) {
+//         vector<int>dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+    
+    // Space optimization
     int tribonacci(int n) {
-        vector<int>dp(n+1,-1);
-        return solve(n,dp);
+        int prev = 1;
+        int prev2 = 1;
+        int prev3 = 0;
+        for(int i=3;i<=n;i++){
+            int curr = prev+prev2+prev3;
+            prev3=prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        if(n==0) return prev3;
+        else if(n==1) return prev2;
+        else return prev;
     }
     
 };
