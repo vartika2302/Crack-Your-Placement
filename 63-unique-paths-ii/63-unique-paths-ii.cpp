1133d817701cb621a4373501e7dddf26db14d9ef
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 03:52:27 +0530

    Time: 4 ms (64.45%), Space: 7.9 MB (42.94%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    // RECURSION
+    int f(int i,int j,vector<vector<int>>& obstacleGrid) {
+        if(i==0 && j==0) return (obstacleGrid[0][0]==1 ? 0 : 1);
+        if(i<0 || j<0 || obstacleGrid[i][j]==1) return 0;
+        int up = f(i-1,j,obstacleGrid);
+        int left = f(i,j-1,obstacleGrid);
+        return up+left;
+    }
+    
+    
+    // MEMOIZATION
+    int fMem(int i,int j,vector<vector<int>>& obstacleGrid,vector<vector<int>>& dp) {
+        if(i==0 && j==0) return (obstacleGrid[0][0]==1 ? 0 : 1);
+        if(i<0 || j<0 || obstacleGrid[i][j]==1) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = fMem(i-1,j,obstacleGrid,dp);
+        int left = fMem(i,j-1,obstacleGrid,dp);
+        return dp[i][j]=up+left;
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m=obstacleGrid.size();
+        int n=obstacleGrid[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        // return f(m-1,n-1,obstacleGrid);
+        return fMem(m-1,n-1,obstacleGrid,dp);
+        
+    }
+};
