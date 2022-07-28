78f28a1d44a8f73888f8009bd9290741322ea60b
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 04:08:12 +0530

    Time: 6 ms (43.07%), Space: 7.9 MB (12.57%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -21,12 +21,32 @@ public:
         return dp[i][j]=up+left;
     }
     
+    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m=obstacleGrid.size();
         int n=obstacleGrid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
         // return f(m-1,n-1,obstacleGrid);
-        return fMem(m-1,n-1,obstacleGrid,dp);
+        // return fMem(m-1,n-1,obstacleGrid,dp);
+        
+        // TABULATION
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    if(obstacleGrid[0][0]==1) dp[0][0]=0;
+                    else dp[0][0]=1;
+                }
+                else if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
         
     }
 };
