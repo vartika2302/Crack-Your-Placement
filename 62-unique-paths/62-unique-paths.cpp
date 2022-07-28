a94d2ff02ef282efbe470a6982156b3e7263a3ef
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 03:17:31 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (44.55%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     
     
-    // RECURSION
+    // RECURSION & MEMOIZATION
     int solveUniquePaths(int i,int j,vector<vector<int>>& dp) {
         if(i==0 && j==0) return 1;
         if(i<0 || j<0) return 0;
@@ -12,8 +12,25 @@ public:
         return dp[i][j]=up+left;
     }
     
+    // TABULATION
+    int solveUniquePathsTab(int m ,int n, vector<vector<int>>& dp) {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=dp[i-1][j];
+                    if(j>0) left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solveUniquePaths(m-1,n-1,dp);
+        // return solveUniquePaths(m-1,n-1,dp);
+        return solveUniquePathsTab(m,n,dp);
     }
 };
