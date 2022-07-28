d50e202a6ea8ef7035d36b512662354c4cb38a3e
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 03:29:28 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (25.59%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -28,9 +28,29 @@ public:
         return dp[m-1][n-1];
     }
     
+    // SPACE OPTIMIZATION
+    int solveUniquePathsSpaceOptimization(int m,int n) {
+        vector<int>prev(n,0);
+        for(int i=0;i<m;i++){
+            vector<int>temp(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) temp[j]=1;
+                else{
+                    int up=0,left=0;
+                    if(i>0) up=prev[j];
+                    if(j>0) left=temp[j-1];
+                    temp[j]=up+left;
+                }
+            }
+            prev=temp;
+        }
+        return prev[n-1];
+    }
+    
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
         // return solveUniquePaths(m-1,n-1,dp);
-        return solveUniquePathsTab(m,n,dp);
+        // return solveUniquePathsTab(m,n,dp);
+        return solveUniquePathsSpaceOptimization(m,n);
     }
 };
