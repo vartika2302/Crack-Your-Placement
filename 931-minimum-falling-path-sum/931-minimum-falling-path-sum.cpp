0fe49d60dc279f4d98c1db8a2bec2266518cd7e6
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-30 01:48:43 +0530

    Time: 12 ms (96.06%), Space: 10.2 MB (52.93%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -39,27 +39,54 @@ public:
         
         
         // Tabulation
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        
+//         vector<vector<int>>dp(n,vector<int>(n,0));
+//         for(int j=0;j<n;j++){
+//             dp[0][j]=matrix[0][j];
+//         }
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 int up=matrix[i][j]+dp[i-1][j];
+//                 int ld=matrix[i][j];
+//                 if(j-1>=0) ld+=dp[i-1][j-1];
+//                 else ld+=1e9;
+//                 int rd=matrix[i][j];
+//                 if(j+1<n) rd+=dp[i-1][j+1];
+//                 else rd+=1e9;
+//                 dp[i][j]=min(up,min(ld,rd));
+//             }
+//         }
+       
+//         for(int j=0;j<n;j++){
+//             mini=min(mini,dp[n-1][j]);
+//         }
+//         return mini;
+        
+        
+        
+        // SPACE OPTIMIZATION
+        vector<int>prev(n,0);
         for(int j=0;j<n;j++){
-            dp[0][j]=matrix[0][j];
+            prev[j] = matrix[0][j];
         }
         for(int i=1;i<n;i++){
+            vector<int>curr(n,0);
             for(int j=0;j<n;j++){
-                int up=matrix[i][j]+dp[i-1][j];
-                int ld=matrix[i][j];
-                if(j-1>=0) ld+=dp[i-1][j-1];
-                else ld+=1e9;
-                int rd=matrix[i][j];
-                if(j+1<n) rd+=dp[i-1][j+1];
-                else rd+=1e9;
-                dp[i][j]=min(up,min(ld,rd));
+                int up = matrix[i][j]+prev[j];
+                int ld = matrix[i][j];
+                if(j-1>=0) ld+=prev[j-1];
+                else ld+=1e8;
+                int rd= matrix[i][j];
+                if(j+1<n) rd+=prev[j+1];
+                else rd+=1e8;
+                curr[j]=min(up,min(ld,rd));
             }
+            prev=curr;
         }
        
         for(int j=0;j<n;j++){
-            mini=min(mini,dp[n-1][j]);
+            mini=min(mini,prev[j]);
         }
         return mini;
-        
     }
 };
