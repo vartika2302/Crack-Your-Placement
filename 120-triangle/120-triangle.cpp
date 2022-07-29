bb53492c92f327e3e6554abbe8d9fc592b4fefd4
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 21:15:06 +0530

    Time: 12 ms (35.42%), Space: 9.4 MB (7.91%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -17,7 +17,7 @@ public:
         return dp[i][j]=min(down,diagonal);
     }
     
-    // TABULATION
+    
      
     
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -27,17 +27,42 @@ public:
         // return f(0,0,m,triangle);
         vector<vector<int>>dp(m,vector<int>(n,-1));
         // return fMem(0,0,m,triangle,dp);
+        
+        
+        
+        //// TABULATION
         // BASE CASE
+        
+        // for(int j=0;j<n;j++){
+        //     dp[m-1][j]=triangle[m-1][j];
+        // }
+        // for(int i=(m-2);i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         int down = triangle[i][j]+dp[i+1][j];
+        //         int diagonal = triangle[i][j]+dp[i+1][j+1];
+        //         dp[i][j]=min(down,diagonal);
+        //     }
+        // }
+        // return dp[0][0];
+        
+        
+        
+        // SPACE OPTIMIZATION
+        vector<int>front(n,-1);
         for(int j=0;j<n;j++){
-            dp[m-1][j]=triangle[m-1][j];
+            front[j]=triangle[n-1][j];
         }
-        for(int i=(m-2);i>=0;i--){
+        
+        for(int i=(n-2);i>=0;i--){
+            vector<int>curr(n,0);
             for(int j=i;j>=0;j--){
-                int down = triangle[i][j]+dp[i+1][j];
-                int diagonal = triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diagonal);
+                int down=triangle[i][j]+front[j];
+                int diagonal = triangle[i][j]+front[j+1];
+                curr[j] = min(down,diagonal);
             }
+            front=curr;
         }
-        return dp[0][0];
+        return front[0];
+        
     }
 };
