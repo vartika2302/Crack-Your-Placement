b9484796e971ad94d8edcef65ffd77920595b6c3
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 06:12:11 +0530

    Time: 17 ms (39.91%), Space: 10.2 MB (28.77%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -19,12 +19,27 @@ public:
         return dp[i][j]=min(left,up);
     }
     
-    
+    // TABULATION
+    int fTab(int m,int n,vector<vector<int>>& grid,vector<vector<int>>& dp) {
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[i][j]=grid[0][0];
+                else{
+                    int up=0,left=0;
+                    up=grid[i][j]+(i>0 ? dp[i-1][j] :1e9+7);
+                    left=grid[i][j]+(j>0 ? dp[i][j-1] :1e9+7);
+                    dp[i][j]=min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,-1));
         // return f(m-1,n-1,grid);
-        return fMem(m-1,n-1,grid,dp);
+        // return fMem(m-1,n-1,grid,dp);
+        return fTab(m,n,grid,dp);
     }
 };
