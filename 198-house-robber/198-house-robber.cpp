a64ab9060d8952b559920a1c8494aeae74330813
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-29 15:39:59 +0530

    Time: 6 ms (16.73%), Space: 7.8 MB (47.09%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -25,13 +25,29 @@ public:
         vector<int>dp(n,-1);
         // return f(n-1,nums);
         // return fMem(n-1,nums,dp);
-        dp[0]=nums[0];
+        
+        // TABULATION
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int pick = nums[i];
+        //     if(i>1) pick+=dp[i-2];
+        //     int notpick = 0+dp[i-1];
+        //     dp[i]=max(pick,notpick);
+        // }
+        // return dp[n-1];
+        
+        // SPACE OPTIMIZATION;
+        int prev2;
+        int prev=nums[0];
         for(int i=1;i<n;i++){
             int pick = nums[i];
-            if(i>1) pick+=dp[i-2];
-            int notpick = 0+dp[i-1];
-            dp[i]=max(pick,notpick);
+            if(i>1) pick+=prev2;
+            int notpick=0+prev;
+            int curr = max(pick,notpick);
+            prev2=prev;
+            prev=curr;
         }
-        return dp[n-1];
+        return prev;
+        
     }
 };
