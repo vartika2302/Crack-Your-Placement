9d6b077cb2ec6951f70438dc5a5c97d2159d9962
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-07-30 01:32:21 +0530

    Time: 15 ms (88.21%), Space: 10.3 MB (50.97%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -29,10 +29,37 @@ public:
         // }
         
         // Memo
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        // vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+        // for(int j=0;j<n;j++){
+        //     mini=min(mini,fMem(n-1,j,n,matrix,dp));
+        // }
+        // return mini;
+        
+        
+        
+        // Tabulation
+        vector<vector<int>>dp(n,vector<int>(n,0));
         for(int j=0;j<n;j++){
-            mini=min(mini,fMem(n-1,j,n,matrix,dp));
+            dp[0][j]=matrix[0][j];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int up=matrix[i][j]+dp[i-1][j];
+                int ld=matrix[i][j];
+                if(j-1>=0) ld+=dp[i-1][j-1];
+                else ld+=1e9;
+                int rd=matrix[i][j];
+                if(j+1<n) rd+=dp[i-1][j+1];
+                else rd+=1e9;
+                dp[i][j]=min(up,min(ld,rd));
+            }
+        }
+       
+        for(int j=0;j<n;j++){
+            mini=min(mini,dp[n-1][j]);
         }
         return mini;
+        
     }
 };
