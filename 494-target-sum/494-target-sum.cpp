6b57c0df289258de1c04f3d7eb9c0a2e61ff7d72
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-01 23:12:59 +0530

    Time: 17 ms (72.90%), Space: 14.5 MB (30.28%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -3,11 +3,12 @@ public:
     
     int f(int idx,vector<int>& nums,int tar,vector<vector<int>>& dp) {
         
-        if(idx==0) {
-            if(tar==0 && nums[idx]==0) return 2;
-            if(tar==0 || tar==nums[idx]) return 1;
-            else return 0;
-        }
+        // if(idx==0) {
+        //     if(tar==0 && nums[idx]==0) return 2;
+        //     if(tar==0 || tar==nums[idx]) return 1;
+        //     else return 0;
+        // }
+        if(idx==-1) return tar==0;
         if(dp[idx][tar]!=-1) return dp[idx][tar];
         int not_pick = f(idx-1,nums,tar,dp);
         int pick = 0;
@@ -18,10 +19,8 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int totsum=0;
         int n = nums.size();
-        int cnt0=0;
         for(int i=0;i<n;i++) {
             totsum+=nums[i];
-            if(nums[i]==0) cnt0++;
         }
         
         int tar = (totsum-target);
