af13ce9338d25764a83240331b33d070dae5551a
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-03 02:36:22 +0530

    Time: 16 ms (83.56%), Space: 18.6 MB (27.49%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -8,6 +8,7 @@ public:
             if(target%coins[0]==0) return 1;
             return 0;
         }
+        if(target==0) return 1;
         if(dp[idx][target]!=-1) return dp[idx][target];
         int not_pick = f(idx-1,coins,target,dp);
         int pick = 0;
@@ -17,8 +18,8 @@ public:
     
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        // return f(n-1,coins,amount,dp);
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        return f(n-1,coins,amount,dp);
         
         
         // TABULATION
@@ -41,20 +42,20 @@ public:
         
         
         // SPACE OPTIMIZATION
-        vector<int>prev(amount+1,0),curr(amount+1,0);
-        for(int t=0;t<=amount;t++){
-            prev[t] = (t%coins[0]==0) ? 1 : 0;
-        }
-        for(int i=1;i<n;i++){
-            for(int t=0;t<=amount;t++){
-                 int not_take = prev[t];
-                int take=0;
-                if(t>=coins[i]) take = curr[t-coins[i]];
-                curr[t] = not_take+take;
-            }
-            prev = curr;
-        }
-        return prev[amount];
+        // vector<int>prev(amount+1,0),curr(amount+1,0);
+        // for(int t=0;t<=amount;t++){
+        //     prev[t] = (t%coins[0]==0) ? 1 : 0;
+        // }
+        // for(int i=1;i<n;i++){
+        //     for(int t=0;t<=amount;t++){
+        //          int not_take = prev[t];
+        //         int take=0;
+        //         if(t>=coins[i]) take = curr[t-coins[i]];
+        //         curr[t] = not_take+take;
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[amount];
         
         
         
