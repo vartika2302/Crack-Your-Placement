2970f3d9e3f8d6621996347c641f76a4fa981a7a
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-02 22:09:13 +0530

    Time: 277 ms (11.53%), Space: 45.2 MB (25.43%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     
+    // RECURSION AND MEMOIZATION
     int f(int idx, vector<int>& coins, int target, vector<vector<int>> &dp) {
         if(idx==0) {
             if(target%coins[0]==0) return target/coins[0];
@@ -13,12 +14,29 @@ public:
         return dp[idx][target] = min(take,not_take);
     }
     
+    
     int coinChange(vector<int>& coins, int amount) {
         
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans = f(n-1,coins,amount,dp);
-        if(ans==1e9) return -1;
-        else return ans;
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // int ans = f(n-1,coins,amount,dp);
+        // if(ans==1e9) return -1;
+        // else return ans;
+        
+        
+        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<=amount;i++){
+            dp[0][i] = (i%coins[0]==0 ? i/coins[0] : 1e9);
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=amount;tar++){
+                int not_take = 0 + dp[i-1][tar];
+                int take = 1e9;
+                if(tar>=coins[i]) take = 1 + dp[i][tar-coins[i]];
+                dp[i][tar] = min(take,not_take);
+            }
+        }
+        return dp[n-1][amount]==1e9 ? -1 : dp[n-1][amount];
     }
 };
