c732b08eef1b8287218685285c06ce23910efff0
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-06 01:21:56 +0530

    Time: 58 ms (54.81%), Space: 18.6 MB (36.65%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,11 +1,13 @@
 class Solution {
 
     private:
+    
+    // RECURSIVE + MEMOIZATION
     int f(int i,int j,string& s,string& t,vector<vector<int>>& dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) {
+        if(s[i-1]==t[j-1]) {
             return dp[i][j] = f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
         }
         return dp[i][j]=f(i-1,j,s,t,dp);
@@ -14,7 +16,14 @@ class Solution {
     int numDistinct(string s, string t) {
         int n = s.size();
         int m = t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,t,dp);
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,s,t,dp);
+        
+        // TABULATION
+//         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+//        //Base cases
+//         for(int i=0;i<=n;i++) dp[i][0] = 1;
+        
+//         return dp[n][m];
     }
 };
