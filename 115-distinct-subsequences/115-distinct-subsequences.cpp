fef5676357b0df73956b1fc52cf311fe85b5d0be
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-06 01:49:17 +0530

    Time: 35 ms (86.31%), Space: 6.3 MB (96.41%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -20,18 +20,19 @@ class Solution {
         // return f(n,m,s,t,dp);
         
         // TABULATION
-        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        // vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+        vector<double>prev(m+1,0),curr(m+1,0);
        //Base cases
-        for(int i=0;i<=n;i++) dp[i][0] = 1;
-        for(int j=1;j<=m;j++) dp[0][j] = 0;
         
+        prev[0]=curr[0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]) dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
-                else dp[i][j] = dp[i-1][j];
+                if(s[i-1]==t[j-1]) curr[j] = prev[j-1]+prev[j];
+                else curr[j] = prev[j];
             }
+            prev=curr;
         }
             
-        return (int)dp[n][m];
+        return (int)prev[m];
     }
 };
