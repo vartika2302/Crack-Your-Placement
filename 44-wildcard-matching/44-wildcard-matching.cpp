ee6bb0184968d9aa2c70a76d2b48550808d458b1
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-07 00:32:33 +0530

    Time: 160 ms (21.38%), Space: 11.3 MB (56.55%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -23,12 +23,35 @@ public:
         int n = s.size();
         int m = p.size();
         // vector<vector<int>>dp(n,vector<int>(m,-1));
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return f(n,m,s,p,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return f(n,m,s,p,dp);
         
         
         // TABULATION
-        // vector<vector<int>>dp(n,vector<int>(m,0));
+        vector<vector<bool>>dp(n+1,vector<bool>(m+1,false));
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++){
+            bool flag = true;
+            for(int k=1;k<=j;k++){
+                if(p[k-1]!='*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[0][j] = flag;
+        }
+        for(int i=1;i<=n;i++){
+            dp[i][0] = false;
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1]=='*') dp[i][j] = dp[i-1][j]||dp[i][j-1];
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
         
     }
 };
