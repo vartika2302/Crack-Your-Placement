8d7fa58d091b251da9c7569dada0924b6e586b16
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-06 23:41:18 +0530

    Time: 74 ms (66.22%), Space: 28 MB (13.37%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -3,17 +3,17 @@ class Solution {
 private:
     bool f(int i, int j, string& s, string& p, vector<vector<int>>& dp) {
         // Base cases
-        if(i<0 && j<0) return true;
-        if(i<0 && j>=0){
-            for(int k = 0;k<=j;k++){
-                if(p[k]!='*') return false;
+        if(i==0 && j==0) return true;
+        if(i==0 && j>0){
+            for(int k = 1;k<=j;k++){
+                if(p[k-1]!='*') return false;
             }
             return true;
         }
-        if(i>=0 && j<0) return false;
+        if(i>0 && j==0) return false;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] || p[j]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
-        if(p[j]=='*') return dp[i][j]=f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
+        if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
+        if(p[j-1]=='*') return dp[i][j]=f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp);
         return false;
     }
     
@@ -22,7 +22,13 @@ public:
     bool isMatch(string s, string p) {
         int n = s.size();
         int m = p.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,s,p,dp);
+        // vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,s,p,dp);
+        
+        
+        // TABULATION
+        // vector<vector<int>>dp(n,vector<int>(m,0));
+        
     }
 };
