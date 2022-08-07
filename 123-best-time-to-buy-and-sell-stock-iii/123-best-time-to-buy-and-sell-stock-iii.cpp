c320ae9fc23caed7ad68015acc847c3941d9324a
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-08 02:20:48 +0530

    Time: 905 ms (31.29%), Space: 207.1 MB (27.32%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -22,7 +22,34 @@ private:
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
-        return f(0,1,2,prices,dp);
+        // vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        // return f(0,1,2,prices,dp);
+        
+        
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        // Note: Base cases will not make any difference as initial value of dp is already 0.
+        // BASE CASE 1
+        for(int i=0;i<n;i++){
+            for(int buy=0;buy<=1;buy++){
+                dp[i][buy][0]=0;
+            }
+        }
+        
+        // BASE CASE 2
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=0;cap<=2;cap++) {
+                dp[n][buy][cap]=0;
+            }
+        }
+        
+        for(int idx=(n-1);idx>=0;idx--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy) dp[idx][buy][cap] = max(-prices[idx]+dp[idx+1][0][cap],dp[idx+1][1][cap]);
+                    else dp[idx][buy][cap] = max(prices[idx]+dp[idx+1][1][cap-1],dp[idx+1][0][cap]);
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
