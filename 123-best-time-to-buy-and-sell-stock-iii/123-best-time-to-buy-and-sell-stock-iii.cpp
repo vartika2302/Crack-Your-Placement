699cc7be89a18265a4a96f74a38ed7a4b2215af1
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-08 02:26:29 +0530

    Time: 278 ms (64.08%), Space: 75.3 MB (78.05%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -26,30 +26,32 @@ public:
         // return f(0,1,2,prices,dp);
         
         
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        // vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+        vector<vector<int>> after(2,vector<int>(3,0)),curr(2,vector<int>(3,0));
         // Note: Base cases will not make any difference as initial value of dp is already 0.
         // BASE CASE 1
         for(int i=0;i<n;i++){
             for(int buy=0;buy<=1;buy++){
-                dp[i][buy][0]=0;
+                curr[buy][0]=0;
             }
         }
         
         // BASE CASE 2
         for(int buy=0;buy<=1;buy++){
             for(int cap=0;cap<=2;cap++) {
-                dp[n][buy][cap]=0;
+                curr[buy][cap]=0;
             }
         }
         
         for(int idx=(n-1);idx>=0;idx--){
             for(int buy=0;buy<=1;buy++){
                 for(int cap=1;cap<=2;cap++){
-                    if(buy) dp[idx][buy][cap] = max(-prices[idx]+dp[idx+1][0][cap],dp[idx+1][1][cap]);
-                    else dp[idx][buy][cap] = max(prices[idx]+dp[idx+1][1][cap-1],dp[idx+1][0][cap]);
+                    if(buy) curr[buy][cap] = max(-prices[idx]+after[0][cap],after[1][cap]);
+                    else curr[buy][cap] = max(prices[idx]+after[1][cap-1],after[0][cap]);
                 }
             }
+            after=curr;
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 };
