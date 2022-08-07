4e588b64333764ae9139e65c55e9cd7d02d1f264
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-08 05:14:45 +0530

    Time: 7 ms (63.10%), Space: 11.9 MB (34.87%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -13,13 +13,24 @@ public:
         // vector<vector<int>>dp(n,vector<int>(2,-1));
         // return f(0,1,prices,n,dp);
         
-        vector<vector<int>>dp(n+2,vector<int>(2,0));
+        
+        // Tabulation
+        // vector<vector<int>>dp(n+2,vector<int>(2,0));
+        // for(int idx=(n-1);idx>=0;idx--){
+        //     for(int buy=0;buy<=1;buy++){
+        //         if(buy) dp[idx][buy] = max(-prices[idx]+dp[idx+1][0],dp[idx+1][1]);
+        //         else dp[idx][buy] = max(prices[idx]+dp[idx+2][1],dp[idx+1][0]);
+        //     }
+        // }
+        // return dp[0][1];
+        
+        // 2nd Tab
+         vector<vector<int>>dp(n+2,vector<int>(2,0));
         for(int idx=(n-1);idx>=0;idx--){
-            for(int buy=0;buy<=1;buy++){
-                if(buy) dp[idx][buy] = max(-prices[idx]+dp[idx+1][0],dp[idx+1][1]);
-                else dp[idx][buy] = max(prices[idx]+dp[idx+2][1],dp[idx+1][0]);
-            }
+            dp[idx][1] = max(-prices[idx]+dp[idx+1][0],dp[idx+1][1]);
+            dp[idx][0] = max(prices[idx]+dp[idx+2][1],dp[idx+1][0]);   
         }
         return dp[0][1];
+        
     }
 };
