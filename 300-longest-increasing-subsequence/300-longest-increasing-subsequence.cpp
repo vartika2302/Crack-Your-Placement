263560e4fe16e877d81ad50b5ecdb7272c602753
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-09 15:14:35 +0530

    Time: 17 ms (79.39%), Space: 10.4 MB (69.86%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -52,17 +52,50 @@ public:
         
         
         // Tabulation - 2nd method
-        vector<int>dp(n,1);
-        for(int idx=1;idx<n;idx++){
-            for(int prev=0;prev<idx;prev++){
-                if(nums[prev]<nums[idx]) dp[idx]=max(dp[idx],1+dp[prev]);
+        // vector<int>dp(n,1),hash(n);
+        // int maxi=1;
+        // int lastIndex=0;
+        // for(int idx=0;idx<n;idx++){
+        //     hash[idx]=idx;
+        //     for(int prev=0;prev<idx;prev++){
+        //         if(nums[prev]<nums[idx] && 1+dp[prev]>dp[idx]){
+        //              dp[idx]=1+dp[prev];
+        //              hash[idx]=prev;
+        //         }
+        //     }
+        //     if(dp[idx]>maxi){
+        //         maxi=dp[idx];
+        //         lastIndex=idx;
+        //     }
+        // }
+        // vector<int>temp;
+        // temp.push_back(nums[lastIndex]);
+        // while(hash[lastIndex]!=lastIndex){
+        //     lastIndex=hash[lastIndex];
+        //     temp.push_back(nums[lastIndex]);
+        // }
+        // reverse(temp.begin(),temp.end());
+        // for(auto it: temp) cout<<(it)<<" ";
+        // cout<<endl;
+        // return maxi;
+        
+        
+        
+        
+        // LIS using Binary Search
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        int len=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]>temp.back()){
+                temp.push_back(nums[i]);
+                len++;
+            }else{
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[ind] = nums[i];
             }
         }
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,dp[i]);
-        }
-        return maxi;
+        return len;
         
     }
     
