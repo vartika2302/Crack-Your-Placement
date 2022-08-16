07f73b961c6e9206263b7e95526206e8c3e4a7b7
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-17 03:05:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (45.44%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.cpp
@@ -5,10 +5,10 @@ public:
         for(auto it: s){
             if(it==letter) freq++;
         }
-        int ans = (double)(freq*100)/s.size();
+        int ans = (float)(freq*100)/s.size();
         int floor_ans = floor(ans);
         int ceil_ans = ceil(ans);
-        cout<<ans<<" "<<floor_ans<<" "<<ceil_ans<<endl;
+        // cout<<ans<<" "<<floor_ans<<" "<<ceil_ans<<endl;
         if(abs(floor_ans-ans)>abs(ceil_ans-ans)) {
             return ceil_ans;
         }
