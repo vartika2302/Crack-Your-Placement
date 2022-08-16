f298cb97c49a0fbad7ca976f4ee224ef05cfb8a6
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-17 04:48:26 +0530

    Time: 31 ms (33.77%), Space: 13.5 MB (57.47%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -15,7 +15,19 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++)
             mpp[nums[i]]++;
-        vector<int>dp(n,-1);
-        return f(n-1,nums,mpp,dp);
+        // vector<int>dp(n,-1);
+        // return f(n-1,nums,mpp,dp);
+        
+        
+        vector<int>dp(n+1,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++) {
+            int not_pick = dp[i-1];
+            int pick=nums[i]*mpp[nums[i]];
+            int temp = mpp[nums[i]]+mpp[nums[i]-1];
+            if(i>=temp) pick+=dp[i-temp];
+            dp[i]=max(pick,not_pick);
+        }
+        return dp[n-1];
     }
 };
