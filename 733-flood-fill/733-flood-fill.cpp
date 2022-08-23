b9bc6fbca0905780d2152fbffc37cbdbcb203fda
Vartika Gupta <88293497+vartika2302@users.noreply.github.com>
2022-08-24 01:47:06 +0530

    Time: 11 ms (80.56%), Space: 14.1 MB (46.49%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,41 +1,26 @@
+// TC - O(N*M)+O(4*N*M) ---> O(N*M)
+// SC - O(N*M)+O(N*M)
 class Solution {
 private:
-    void dfs(int sr,int sc,vector<vector<int>>& image,int color,int prev_color,vector<vector<int>>& vis,int n,int m) {
-        vis[sr][sc]=1;
-        image[sr][sc]=color;
-        // left
-        if(sc-1>=0 && sc-1<m && image[sr][sc-1]==prev_color && !vis[sr][sc-1]){
-            vis[sr][sc-1]=1;
-            image[sr][sc-1]=color;
-            dfs(sr,sc-1,image,color,prev_color,vis,n,m);
-        }
-        // right
-        if(sc+1>=0 && sc+1<m && image[sr][sc+1]==prev_color && !vis[sr][sc+1]){
-            vis[sr][sc+1]=1;
-            image[sr][sc+1]=color;
-            dfs(sr,sc+1,image,color,prev_color,vis,n,m);
-        }
-        // top
-        if(sr-1>=0 && sr-1<n && image[sr-1][sc]==prev_color && !vis[sr-1][sc]){
-            vis[sr-1][sc]=1;
-            image[sr-1][sc]=color;
-            dfs(sr-1,sc,image,color,prev_color,vis,n,m);
-        }
-        // bottom
-        if(sr+1>=0 && sr+1<n && image[sr+1][sc]==prev_color && !vis[sr+1][sc]){
-            vis[sr+1][sc]=1;
-            image[sr+1][sc]=color;
-            dfs(sr+1,sc,image,color,prev_color,vis,n,m);
+    void dfs(int row, int col, vector<vector<int>>&image, vector<vector<int>>& ans, int color, int initialColor,int drow[], int dcol[],int n,int m){
+        ans[row][col]=color;
+        for(int i=0;i<4;i++){
+            int newRow = row+drow[i];
+            int newCol = col+dcol[i];
+            if(newRow>=0 && newCol>=0 && newRow<n && newCol<m && image[newRow][newCol]==initialColor && ans[newRow][newCol]!=color){
+                dfs(newRow,newCol,image,ans,color,initialColor,drow,dcol,n,m);
+            }
         }
     }
-    
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n = image.size();
         int m = image[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        int prev_color=image[sr][sc];
-        dfs(sr,sc,image,color,prev_color,vis,n,m);
-        return image;
+        vector<vector<int>>ans = image;
+        int initialColor = image[sr][sc];
+        int drow[] = {-1,0,+1,0};
+        int dcol[] = {0,-1,0,+1};
+        dfs(sr,sc,image,ans,color,initialColor,drow,dcol,n,m);
+        return ans;
     }
 };
